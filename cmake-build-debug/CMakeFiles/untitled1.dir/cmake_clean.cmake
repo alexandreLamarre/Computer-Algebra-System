7cdf37cedc815e09f996a7c28196288fba90c706
alexandreLamarre <alex7285@gmail.com>
2020-10-09 11:28:22 -0400

    Inital commit for C Computer Algebra System library.

---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled1.dir/library.c.obj"
+  "libuntitled1.a"
+  "libuntitled1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef UNTITLED1_LIBRARY_H
+#define UNTITLED1_LIBRARY_H
+
+void hello(void);
+
+#endif //UNTITLED1_LIBRARY_H
